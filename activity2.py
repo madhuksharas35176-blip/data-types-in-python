8329f01c55225cb03ea5c69abab980ed71dcaeb1
madhuksharas35176-blip <madhuksharas_35176@chettinadvidyashram.org>
2026-03-26 17:47:12 +0530

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,23 @@
+name = "penguin"
+age = 15
+is_student = True
+weight = 38.5
+print("Name:",name)
+print("Data type of name is",type(name) ) 
+print("Age :", age)
+print("Data Type of Age is", type(age))
+print("is_student :", is_student)
+print("Data Type of is_student is", type(is_student))
+print("weight :", weight)
+print("Data Type of weight is", type(weight))
+print("\n After Type Casting....")
+age = str(age)
+print(age)
+print("Data Type of weight is", type(weight))
+print = int(weight)
+print(weight)
+print("Data Type of age is",type(age))
+weight(weight)
+print("Data Type of Weight is",type(weight))
+      
+
